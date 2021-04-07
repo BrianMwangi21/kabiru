e6089f74fe7d371bb0c69b6308e3c742006ed07b
Brian Mwangi <mwangibrian8@gmail.com>
2021-04-07 22:22:12 +0300

    Fixed grammar error

--- src/portfolio.js
+++ src/portfolio.js
@@ -342,7 +342,7 @@ const experience = {
 const projectsHeader = {
   title: "Projects",
   description:
-    "My projects make use of a vast variety of latest technology tools. My best experience is to create NodeJS Backend Projects, Python Scripts, and React Project. I have also dipped my feet into some AI and blockchain as well. Below are some of my projects. Note that not all of the mentioned projects are on GitHub yet.",
+    "My projects make use of a vast variety of the latest technology tools. My best experience is to create NodeJS Backend Projects, Python Scripts, and React Projects. I have also dipped my feet into some AI and blockchain as well and I am loving it. Check them out below and remember - more are coming soon 👌",
   avatar_image_path: "projects_image.svg",
 };
 
